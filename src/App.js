412a6b8af5da75c021b877032d7fe0b2974e0017
Hoang-SMD <hoang.doan@smartdev.com>
2023-12-21 16:38:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="noel">
+        <div></div>
+        <div className="leaf-layer">
+          <div className="item left">
+            <div className="leaves">Giáng</div>
+          </div>
+          <div className="item right">
+            <div className="leaves">Giáng</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
